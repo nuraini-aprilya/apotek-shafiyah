4f91b5d31bcab08aa764a43b676da462d8e5e005
danikaharu <dani.kaharu17@gmail.com>
2024-03-12 14:23:41 +0800

    change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CustomerSeeder::class,
+            TypeSeeder::class,
             RoleSeeder::class,
             ProfileSeeder::class
         ]);
