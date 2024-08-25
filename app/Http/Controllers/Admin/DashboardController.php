6d9e74e33267aeef322a928dd091f66f226fc602
danikaharu <dani.kaharu17@gmail.com>
2024-08-25 21:39:01 +0800

    add dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $orders = Order::with('customer')->latest()->limit(5)->get();
+
+        return view('admin.dashboard.index', compact('orders'));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -29,77 +29,24 @@
                         <thead>
                             <tr>
                                 <th>No</th>
-                                <th>ID</th>
-                                <th>Nama</th>
-                                <th>Produk</th>
-                                <th>Aksi</th>
+                                <th>Customer</th>
+                                <th>Tanggal Pembelian</th>
+                                <th>Status</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>John Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-success">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Alexander Pierce</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-warning">Pending</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Bob Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-primary">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Mike Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-danger">Denied</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>Jim Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-success">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>6</td>
-                                <td>Victoria Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-warning">Pending</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>7</td>
-                                <td>Michael Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-primary">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
-                            <tr>
-                                <td>8</td>
-                                <td>Rocky Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="tag tag-danger">Denied</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback
-                                    doner.</td>
-                            </tr>
+                            @forelse($orders as $order)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $order->customer->user->username ?? 'Kasir' }}</td>
+                                    <td>{{ $order->created_at->format('d F Y H:i:s') }}</td>
+                                    <td><span class="tag tag-success">{{ $order->status() }}</span></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="4"></td>
+                                </tr>
+                            @endforelse
                         </tbody>
                     </table>
                 </div>
@@ -110,7 +57,7 @@
     </div>
     <!-- Main row -->
 
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header bg-info">
@@ -230,7 +177,7 @@
             <!-- /.card -->
         </div>
         <!-- /.col -->
-    </div>
+    </div> --}}
     <!-- /.row -->
     <!-- /.row (main row) -->
 @endsection
